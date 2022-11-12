1fa9457407dd5bbbd8ea7d972b68c169c9af7a9d
1boey <114894417+1boey@users.noreply.github.com>
2022-11-12 22:54:45 +0800

    2048 (v1.2) ----------------------------------------- --Changes-- Fixed major bug where cells would generate regardless of whether there were cells that could be added or not.

--- src/main/java/com/coursework/cw1dms/GameScene.java
+++ src/main/java/com/coursework/cw1dms/GameScene.java
@@ -20,6 +20,10 @@ class GameScene{
     private Cell[][] cells = new Cell[n][n];
     private Group root;
     private long score = 0;
+    private long sc1=0; // another variable that worked side by side with boolean added.
+    private boolean added=false; // variable added to check whether the score was incremented
+    private int ll,lr,ul,dl=0; // all variables here were used in cooperation with their methods , so if moveLeft returned 1 then ll.moveLeft would ==1
+    public int x,y=0;
 
     static void setN(int number) {
         n = number;
@@ -80,7 +84,7 @@ class GameScene{
     }
 
     private int haveEmptyCell() {
-        for (int i = 0; i < n; i++) {
+        for (int i= 0 ; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (cells[i][j].getNumber() == 0)
                     return 1;
@@ -146,38 +150,53 @@ class GameScene{
 
     // removed cells[i][j].setModify from methods: moveLeft(), moveRight(), moveUp(), moveDown() -> pushed to sumCellNumberstoScore(), so that
     // it would only be set back to false after I add to score
-    private void moveLeft() {
+    private int moveLeft() {
         for (int i = 0; i < n; i++) {
             for (int j = 1; j < n; j++) {
-                moveHorizontally(i, j, passDestination(i, j, 'l'), -1);
+                x=moveHorizontally(i, j, passDestination(i, j, 'l'), -1);
+                if(x==1){  // if moveHorizontally were to return 1
+                    y=x; //then y would =1
+                }
             }
         }
+        return y; // then this method will return 1 , and this logic would reoccur with every up, down, left, right else return 0
     }
 
-    private void moveRight() {
+    private int moveRight() {
         for (int i = 0; i < n; i++) {
             for (int j = n - 1; j >= 0; j--) {
-                moveHorizontally(i, j, passDestination(i, j, 'r'), 1);
+                x=moveHorizontally(i, j, passDestination(i, j, 'r'), 1);
+                if(x==1){ // if moveHorizontally were to return 1
+                    y=x; // then y would =1
+                }
             }
         }
+        return y; // then this method will return 1 , and this logic would reoccur with every up, down, left, right
     }
 
-    private void moveUp() {
+    private int moveUp() {
         for (int j = 0; j < n; j++) {
             for (int i = 1; i < n; i++) {
-                moveVertically(i, j, passDestination(i, j, 'u'), -1);
+                x=moveVertically(i, j, passDestination(i, j, 'u'), -1); // if moveVertically were to return 1
+                if(x==1){ //then x would =1
+                    y=x; // which in turn make y=1 as well
+                }
+
             }
         }
-
+        return y;
     }
 
-    private void moveDown() {
+    private int moveDown() {
         for (int j = 0; j < n; j++) {
             for (int i = n - 1; i >= 0; i--) {
-                moveVertically(i, j, passDestination(i, j, 'd'), 1);
+                x=moveVertically(i, j, passDestination(i, j, 'd'), 1); // if moveVertically were to return 1
+                if(x==1){ //then x would =1
+                    y=x; // which in turn make y=1 as well
+                }
             }
         }
-
+        return y;
     }
 
     private boolean isValidDesH(int i, int j, int des, int sign) {
@@ -190,13 +209,28 @@ class GameScene{
         return false;
     }
 
-    private void moveHorizontally(int i, int j, int des, int sign) {
+
+    //below method and the method moveVertically have been fixed, cells would only spawn iff there's a possibility of adding or if two cells have
+    //already been added.
+    private int moveHorizontally(int i, int j, int des, int sign) {
+        int x,y;
+        x=y=0; // x and y variables are cleared to store the b
         if (isValidDesH(i, j, des, sign)) {
             cells[i][des+sign].setModify(true);   //ref to moveVertically
             cells[i][j].adder(cells[i][des + sign]);
         } else if (des != j) {
+            x=cells[i][j].getNumber(); // variable x would the value of cells[i][j].getNumber
+            y=cells[i][des].getNumber();
             cells[i][j].changeCell(cells[i][des]);
+            //if the position of cells[i][j].getNumber() != cells[i][des].getNumber(), that would mean that the cells have
+            //legally moved in the boundary of n*n;
+            if (x!=cells[i][j].getNumber() && y!=cells[i][des].getNumber() || cells[i][j].getNumber()!=0){ // cells[i][j].getNumber()!=0 was added
+                // because originally the program would not spawn as the majority of the rows/columns of the board was already filled, after adding this, it would
+                // spawn legally(only when added or when there's a possibility of adding)
+                return 1;
+            }
         }
+        return 0;
     }
 
     private boolean isValidDesV(int i, int j, int des, int sign) {
@@ -208,13 +242,22 @@ class GameScene{
         return false;
     }
 
-    private void moveVertically(int i, int j, int des, int sign) {
+    private int moveVertically(int i, int j, int des, int sign) {
         if (isValidDesV(i, j, des, sign)) {
             cells[des+sign][j].setModify(true);      // replaced [des+sign] instead of cells[des] , and swapped it to the top, this was to capture
             cells[i][j].adder(cells[des + sign][j]); // everytime the adder method was called to += to score
         } else if (des != i) {
+            int a=cells[i][j].getNumber(); // variable "a" would the value of cells[i][j].getNumber
+            int b=cells[des][j].getNumber(); // variable "b" would store the value of cells[des][j].getNumber()
             cells[i][j].changeCell(cells[des][j]);
+
+            //if the position of cells[i][j].getNumber() != cells[i][des].getNumber(), that would mean that the cells have
+            //legally moved in the boundary of n*n;
+            if (a != cells[i][j].getNumber() && b != cells[des][j].getNumber()  || cells[i][j].getNumber()!=0){
+                return 1;
+            }
         }
+        return 0;
     }
 
     private boolean haveSameNumberNearly(int i, int j) {
@@ -231,7 +274,7 @@ class GameScene{
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (haveSameNumberNearly(i, j)) {
-                    return false;
+                 return false;
                 }
             }
         }
@@ -251,6 +294,7 @@ class GameScene{
     }
 
     void game(Scene gameScene, Group root, Stage primaryStage, Scene endGameScene, Group endGameRoot) {
+
         this.root = root;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -279,29 +323,51 @@ class GameScene{
         gameScene.addEventHandler(KeyEvent.KEY_PRESSED, key -> Platform.runLater(() -> { //refactored code to make it simpler
             int haveEmptyCell;
             if (key.getCode() == KeyCode.DOWN) {
-                GameScene.this.moveDown();
+                this.dl=GameScene.this.moveDown(); // this.dl will initially =0;
             } else if (key.getCode() == KeyCode.UP) {
-                GameScene.this.moveUp();
+                this.ul=GameScene.this.moveUp(); // this.ul will initially =0;
             } else if (key.getCode() == KeyCode.LEFT) {
-                GameScene.this.moveLeft();
+                this.ll=GameScene.this.moveLeft(); // this.ll will initially =0;
             } else if (key.getCode() == KeyCode.RIGHT) {
-                GameScene.this.moveRight();
+                this.lr=GameScene.this.moveRight(); // this.lr will initially =0;
+            }
+            sc1=score;
+            GameScene.this.sumCellNumbersToScore();
+            scoreText.setText(score + "");
+            if (sc1!=score){ // variable sc1 will check whether the score has been incremented or not --> sc1 would always be lesser than score,
+                             // because it would only start incrementing after adding two cells n+1 times. For example cells 2 and 2 would generate a score
+                             // of 4, but sc1 will remain 0.
+
+                added=true;  // sc1 is not equals to true, that would mean that there was a change in scores
+            }else{
+                added=false;
             }
             haveEmptyCell = GameScene.this.haveEmptyCell();
             if (haveEmptyCell == -1) {
                 if (GameScene.this.canNotMove()) {
                     primaryStage.setScene(endGameScene);
-
                     EndGame.getInstance().endGameShow(endGameScene, endGameRoot, primaryStage, score);
                     root.getChildren().clear();
                     score = 0;
                 }
             } else if(haveEmptyCell == 1) {
                 if (key.getCode() == KeyCode.DOWN ||key.getCode() == KeyCode.UP || key.getCode() == KeyCode.LEFT || key.getCode() == KeyCode.RIGHT) {
-                    //added key.getCode()^ to ensure that random cells WOULD spawn and scores COULD increment only when keys are pressed
-                    GameScene.this.randomFillNumber(); //removed int turn
-                    GameScene.this.sumCellNumbersToScore();
-                    scoreText.setText(score + "");
+                    if (added){ // if two cells were added, generate a cell
+                        GameScene.this.randomFillNumber();
+                    }else { //else check whether any of the below methods returned a 1
+                        if (this.ll == 1 || this.lr == 1 || this.ul == 1 || this.dl == 1) { //when either moveUp/moveDown/moveLeft/moveRight returns 1,
+                            GameScene.this.randomFillNumber();// then generate new cells.
+                        }
+                    }
+                    // after generating new cells, set all back to 0 -> so that it can ready for detection of whether ( cells have been added or whether there are
+                    // empty spaces
+                    this.ll=0;
+                    this.lr=0;
+                    this.ul=0;
+                    this.dl=0;
+                    this.x=0;
+                    this.y=0;
+
                 }
             }
         }));
