73cfb00c9e0f082e948363afc77ed6af267f796d
1boey <114894417+1boey@users.noreply.github.com>
2022-11-22 11:41:02 +0800

    2048(v1.4)
    Big Changes(Version 1.4)
    --------------------------
    
    -Changed the entire endgame scene and re-implemented it in javafx.
    
    -The addition of new file: EndGameScene.fxml which is being used for the EndGameScene
    
    Small Changes(Version 1.4)
    --------------------------
    
    -In class Main, try and catch was wrapped around the code that was responsible for initiating the endgame scene, as the new endgame scene was done in SceneBuilder.
    
    - The endgame scene now has working exit button, and back to main-menu button
    
    - Cleaned up Main.java code.

--- src/main/java/com/coursework/cw1dms/Controller.java
+++ src/main/java/com/coursework/cw1dms/Controller.java
@@ -16,26 +16,32 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class Controller extends Main {
+public class Controller extends EndGame{
+
     public Stage stage;
-
-    private Scene scene;
-
-    private Parent root;
     static final int WIDTH = 900;
     static final int HEIGHT = 700;
 
     @FXML
     private Pane pane;
 
+    private static Controller singleInstance = null;
+
     @FXML
-    private ColorPicker colorPicker;
+    public ColorPicker colorPicker;
 
-    public void changeColor(ActionEvent event) {
+    public Color color;
 
-        Color color = colorPicker.getValue();
+    public Group gameRoot = new Group();
+
+
+    public void changeColor(ActionEvent event) throws IOException {
+        color = colorPicker.getValue();
         pane.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
+
     }
+
+
 
     public void switchScenes(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("Menu.fxml")));
@@ -44,19 +50,15 @@ public class Controller extends Main {
         Scene gameScene = new Scene(gameRoot, WIDTH, HEIGHT, colorPicker.getValue());
 
         Rectangle backgroundOfMenu = new Rectangle(240, 120, Color.rgb(120, 120, 120, 0.2));
-        backgroundOfMenu.setX(WIDTH / 2 - 120);
+        backgroundOfMenu.setX((float)WIDTH / 2 - 120);
         backgroundOfMenu.setY(180);
 
-
         Rectangle backgroundOfMenuForPlay = new Rectangle(240, 140, Color.rgb(120, 20, 100, 0.2));
-        backgroundOfMenuForPlay.setX(WIDTH / 2 - 120);
+        backgroundOfMenuForPlay.setX((float)WIDTH / 2 - 120);
         backgroundOfMenuForPlay.setY(180);
 
-        Group endgameRoot = new Group();
-        Scene endGameScene = new Scene(endgameRoot, WIDTH, HEIGHT, colorPicker.getValue());
-
         GameScene startGame = new GameScene();
-        startGame.game(gameScene,gameRoot,stage,endGameScene,endgameRoot);
+        startGame.game(gameScene,gameRoot,stage,color);
 
         stage.setScene(gameScene);
         stage.setResizable(false);
--- src/main/java/com/coursework/cw1dms/EndGame.java
+++ src/main/java/com/coursework/cw1dms/EndGame.java
@@ -3,11 +3,14 @@ package com.coursework.cw1dms;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 import org.jetbrains.annotations.NotNull;
 import java.io.IOException;
@@ -19,11 +22,12 @@ public class EndGame{
     public Button exitButton;
     public Button goBacktoMainMenu;
     @FXML
-    private Pane exitPane;
+    public AnchorPane exitPane;
     @FXML
     public Label finalScore;
     private Stage endStage;
     private Scene endScene;
+
     public EndGame(){
 
     }
@@ -33,22 +37,17 @@ public class EndGame{
             singleInstance= new EndGame();
         return singleInstance;
     }
-
-    public void endGameShow(@NotNull Stage primaryStage, long score) throws IOException {
-
-            FXMLLoader loader= new FXMLLoader(getClass().getResource("EndGameScene.fxml")); // EndGameScene now done in SceneBuilder
-
+    public void endGameShow(@NotNull Stage primaryStage, long score, Color color) throws IOException {
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("EndGameScene.fxml"));
             Parent endRoot = loader.load(); //load the loader
             EndGame eg = loader.getController(); //use loader to get controller to get label( if this is not done, this.finalscore will be null)
             eg.finalScore.setText(""+score);
-
+            eg.exitPane.setBackground(new Background(new BackgroundFill(color, CornerRadii.EMPTY, Insets.EMPTY)));
             Scene endScene = new Scene(endRoot);
             primaryStage.setScene(endScene);
             primaryStage.setResizable(false);
             primaryStage.show();
     }
-
-
 
     public void exitGame(ActionEvent event) { //implementation of exitGame Button
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
